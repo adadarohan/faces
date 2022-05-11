5d13b58fdbb6a873970313771817a00b47709b3d
Rohan Kapur <adada.rohan@gmail.com>
2022-05-11 10:13:45 +0530

    fixed lazy loading bug

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const fake_url = fake[Math.floor(Math.random() * fake.length)];
 const real_url = real[Math.floor(Math.random() * real.length)];
 const eles = ['correct', 'wrong']
 const last_ele = eles[Math.floor(Math.random() * eles.length)];
 
-document.getElementById('wrong_img').src = fake_url;
+document.getElementById('wrong_img').src = 'https://thispersondoesnotexist.com/image';
 document.getElementById('correct_img').src = real_url;
+document.getElementById('correct_img').onload = () => {
+    document.getElementById('pics').style.display = 'flex';
+}
+
 console.log(last_ele)
 document.getElementById(last_ele).classList.add('order-last');
--- unsplash.py
+++ unsplash.py
@@ -4,8 +4,8 @@ import requests
 x = 1
 final_urls = []
 
-while len(final_urls) < 1000 :
-    data=requests.get(f"https://api.unsplash.com/search/photos?query=portrait%20headshot%20human&client_id=C3kCVQd176QdBI_BxdDl1P4AdqJlBOLwOt4GuuiIRGc&per_page=1000&orientation=squarish&page={x}").json()
+while len(final_urls) < 200 :
+    data=requests.get(f"http://api.unsplash.com/search/photos?query=faces&client_id=C3kCVQd176QdBI_BxdDl1P4AdqJlBOLwOt4GuuiIRGc&per_page=1000&page={x}", verify=False).json()
 
     # Opening JSON file
     #f = open('try1.json')
@@ -17,5 +17,5 @@ while len(final_urls) < 1000 :
     x = x+1
 print(len(final_urls))
 
-with open('unsplash.json', 'w') as f:
+with open('unsplash-faces.json', 'w') as f:
     json.dump(final_urls,f)
